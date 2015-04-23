b94ec134ea1784c3e76075a1bac27fe8cfcbfcfd
MiltonLab <1000ton.lab@gmail.com>
2015-04-23 12:49:55 -0500

    Create twitter_streaming.py

---
+++ socialysis/twitter_streaming.py
@@ -0,0 +1,18 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+class StdOutListener(StreamListener):
+    def on_data(self, data):
+        print data
+        return True
+    def on_error(self, status):
+        print status
+l=StdOutListener()
+stream = Stream(auth,l)
+stream.filter(track=['python','java','c++'])
